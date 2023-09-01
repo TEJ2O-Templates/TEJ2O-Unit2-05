58f5d3291e1b424cd3bd6e2f72cbe939ba2edb18
Patrick Coxall <mr.coxall@mths.ca>
2023-09-01 13:18:47 -0400

    update

--- main.ts
+++ main.ts
@@ -13,9 +13,12 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
+  // get light value
   lightValue = input.lightLevel()
   lightValuePercentage = (lightValue / 255) * 100
   lightValuePercentage = Math.round(lightValuePercentage)
+  
+  // output answer
   basic.clearScreen()
   basic.showString('Percentage light is: ' + lightValuePercentage.toString() + '%.')
 })
