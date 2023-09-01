0cc9355d70008e23582cfb51e3a863adb709a3e2
Patrick Coxall <mr.coxall@mths.ca>
2023-09-01 13:15:11 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,20 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program return percentage of light
 */
 
-basic.showString('Hello, World!')
+// our variable for a random number
+let lightValue: number
+let lightValuePercentage: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  lightValue = input.lightLevel()
+  lightValuePercentage = (lightValue / 255) * 100
+  lightValuePercentage = Math.round(lightValuePercentage)
+  basic.clearScreen()
+  basic.showString('Percentage light is: ' + lightValuePercentage.toString() + '%.')
+})
